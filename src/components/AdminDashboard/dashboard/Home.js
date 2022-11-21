6c7acaf6a33e5b12dd7637338016363ce630ca1a
mogulafric <info@mogulafric.com>
2022-11-21 22:39:56 +0300

    RM

--- src/components/AdminDashboard/dashboard/Home.js
+++ src/components/AdminDashboard/dashboard/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "../../../axios";
 const Home = () => {
   const [countSuppliers, setCountSuppliers] = useState(0);
   const [countItems, setCountItems] = useState(0);
